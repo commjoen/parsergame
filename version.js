2db06d1e100a5d93c79c283cb9a59f4019141af7
GitHub Action <action@github.com>
2025-09-28 22:17:33 +0000

    chore: bump version to v1.1.1 [skip release]

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 // Version information for Parser Game
 // This file is automatically updated by the build process
-const APP_VERSION = '1.1.0';
+const APP_VERSION = '1.1.1';
 
 // Export for use in other modules
 if (typeof module !== 'undefined' && module.exports) {
